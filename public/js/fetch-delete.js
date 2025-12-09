f9a0dd4162699d3c95838d72e2b86bda69e39892
yahyacoder7 <yahya.exe07@gmail.com>
2025-12-09 13:45:19 -0800

    fix: correct form ID and enhance delete button functionality

--- public/js/fetch-delete.js
+++ public/js/fetch-delete.js
@@ -1,11 +1,21 @@
-const regForm = document.getElementById('regFrom')
+const regForm = document.getElementById('formReg')
 const btnDelete = document.getElementById('delete-btn')
 
-btnDelete.addEventListener('click', (event) => {
+btnDelete.addEventListener('click', async (event) => {
     event.preventDefault()
     const formData = new FormData(regForm)
-    const dataToSend = Object.fromEntries(formData)
+    const data = Object.fromEntries(formData)
 
-    console.log(dataToSend)
-    
+    console.log(data)
+
+    const url = `/delete-item/${data.type}/${data.id}`;
+    try {
+
+        const resbone = await fetch(url, { method: 'DELETE' })
+        const result = await resbone.json()
+        result.success ? alert(result.message) : alert('Error:' + result.message)
+
+    } catch (err) {
+        console.error('Error:', err);
+    }
 })
